eea8073db233e80cdc703a31ba7689911b6e7a05
heewonn09 <heewonn09@naver.com>
2026-01-27 12:43:10 +0900

    Revert "[FEAT] Safety(core): enums + entities + repositories #10"

--- src/main/java/com/backend/nova/safety/enums/SafetyReason.java
+++
@@ -1,8 +0,0 @@
-package com.backend.nova.safety.enums;
-
-public enum SafetyReason {
-    FIRE_SMOKE,
-    HEAT,
-    MANUAL_LOCK,
-    MANUAL_UNLOCK
-}
--- src/main/java/com/backend/nova/safety/enums/SensorType.java
+++
@@ -1,6 +0,0 @@
-package com.backend.nova.safety.enums;
-
-public enum SensorType {
-    SMOKE,
-    HEAT
-}
