9f470f23d16e5fbef1513942bba69bdaad902e04
heewonn09 <heewonn09@naver.com>
2026-01-28 19:26:20 +0900

    [feat] CI test

--- src/main/java/com/backend/nova/config/SecurityConfig.java
+++ src/main/java/com/backend/nova/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
                 // 인가 처리
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/member/login", "/api/member/signup", "/api/resident/verify").permitAll()
-                        .requestMatchers("/api", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api", "/swagger-ui/**", "/v3/api-docs/**","/ai/chat/**").permitAll()
+
                         .anyRequest().authenticated()
                 )
                 // 커스텀 필터 설정 JwtFilter 선행 처리
