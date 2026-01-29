677ce1a6bf949911ae9675c515cea79c571da56d
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-01-29 15:24:00 +0900

    [CHOR] 관리자 로그인 테스트 클래스 이름 수정

--- src/test/java/com/backend/nova/admin/controller/AdminControllerIntegrationTest.java
+++ src/test/java/com/backend/nova/admin/controller/AdminControllerIntegrationTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.UUID;
 
