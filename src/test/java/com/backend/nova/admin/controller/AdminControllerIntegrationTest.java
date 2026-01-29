2248031ac1b8684af8b7d99890489a0a10252a34
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-01-29 21:09:56 +0900

    [REFACTOR] 관리자 테스트 아파트 UUID 재설정

--- src/test/java/com/backend/nova/admin/controller/AdminControllerIntegrationTest.java
+++ src/test/java/com/backend/nova/admin/controller/AdminControllerIntegrationTest.java
@@ -64,8 +64,8 @@ class AdminControllerIntegrationTest {
      */
     private Apartment createApartment() {
         Apartment apartment = Apartment.builder()
-                .name("테스트 아파트")
-                .address("서울시 테스트구 테스트동") //  필수
+                .name("테스트 아파트-" + UUID.randomUUID())
+                .address("서울시 테스트구 테스트동")
                 .build();
         return apartmentRepository.save(apartment);
     }
