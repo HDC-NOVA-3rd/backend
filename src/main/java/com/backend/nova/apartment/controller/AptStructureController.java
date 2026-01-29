53c099dcd86bb39e477054378e6580ae405f4c4b
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2026-01-29 17:26:02 +0900

    [DOCS] 추가한 컨트롤러에 대해 Swagger 명세 설명 추가

--- src/main/java/com/backend/nova/apartment/controller/AptStructureController.java
+++ src/main/java/com/backend/nova/apartment/controller/AptStructureController.java
@@ -1,6 +1,8 @@
 package com.backend.nova.apartment.controller;
 
 import com.backend.nova.apartment.service.AptStructureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import com.backend.nova.apartment.dto.ApartmentStructure.*;
 
 import java.util.List;
 
+@Tag(name = "아파트 동 호수 정보 조회 ", description = "아파트 단지, 동, 호로 이어지는 정보를 조회하는 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/apartment")
@@ -18,6 +21,10 @@ public class AptStructureController {
     private final AptStructureService aptStructureService;
 
     // 아파트 목록 조회 API
+    @Operation(
+            summary = "전체 아파트 목록 조회",
+            description = "서비스에 등록된 모든 아파트 단지의 기본 정보(ID, 이름 등)를 리스트 형태로 반환합니다."
+    )
     @GetMapping
     public ResponseEntity<List<ApartmentResponse>> getApartmentList() {
         List<ApartmentResponse> apartments = aptStructureService.getApartmentList();
@@ -25,6 +32,10 @@ public class AptStructureController {
     }
 
     // 동 목록 조회 API (아파트 ID 기준)
+    @Operation(
+            summary = "특정 아파트의 동(Dong) 목록 조회",
+            description = "선택한 아파트 ID에 소속된 모든 동(예: 101동, 102동) 리스트를 반환합니다."
+    )
     @GetMapping("/{apartmentId}/dong")
     public ResponseEntity<List<DongResponse>> getDongList(@PathVariable Long apartmentId) {
         List<DongResponse> dongs = aptStructureService.getDongListByApartmentId(apartmentId);
@@ -32,6 +43,10 @@ public class AptStructureController {
     }
 
     // 호 목록 조회 API (동 ID 기준)
+    @Operation(
+            summary = "특정 동의 호(Ho) 목록 조회",
+            description = "선택한 동 ID에 소속된 모든 호(예: 101호, 102호) 리스트를 반환합니다."
+    )
     @GetMapping("/dong/{dongId}/ho")
     public ResponseEntity<List<HoResponse>> getHoList(@PathVariable Long dongId) {
         List<HoResponse> hos = aptStructureService.getHoListByDongId(dongId);
