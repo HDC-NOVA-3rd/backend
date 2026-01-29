4cbd5ace986c4d48689fbc9f5ddd1c10d678fadd
AhnCSK <ahncsk00@naver.com>
2026-01-29 23:01:47 +0900

    [FIX] Apartment 엔티티 길이 추가

--- src/main/java/com/backend/nova/apartment/entity/Apartment.java
+++ src/main/java/com/backend/nova/apartment/entity/Apartment.java
@@ -14,10 +14,10 @@ public class Apartment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 255)
     private String address;
 
     @Column(length = 50)
