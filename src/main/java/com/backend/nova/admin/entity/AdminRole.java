4af309515907cb540223ab87a35b5e80a3348711
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-01-30 13:55:42 +0900

    [FEAT] AdminRole 추가

--- src/main/java/com/backend/nova/admin/entity/AdminRole.java
+++ src/main/java/com/backend/nova/admin/entity/AdminRole.java
@@ -3,7 +3,9 @@ package com.backend.nova.admin.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum AdminRole implements GrantedAuthority {
-    ADMIN, SUPER_ADMIN;
+    SUPER_ADMIN,     // 최고 관리자
+    MANAGER,         // 배정 담당자
+    ADMIN;            // 일반 관리자 (조회만)
 
     @Override
     public String getAuthority() {
