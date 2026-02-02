ff22eb425a00b7c371da876b91127389386a0f09
heewonn09 <heewonn09@naver.com>
2026-02-02 16:18:03 +0900

    [feat] 최근 환경 데이터 조회 메서드

--- src/main/java/com/backend/nova/homeEnvironment/repository/RoomEnvLogRepository.java
+++ src/main/java/com/backend/nova/homeEnvironment/repository/RoomEnvLogRepository.java
@@ -1,13 +1,21 @@
 package com.backend.nova.homeEnvironment.repository;
 
 import com.backend.nova.homeEnvironment.entity.RoomEnvLog;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RoomEnvLogRepository extends JpaRepository<RoomEnvLog, Long> {
 
 
+
+    List<RoomEnvLog> findByRoom_IdAndSensorTypeOrderByRecordedAtDesc(
+            Long roomId,
+            String sensorType,
+            Pageable pageable
+    );
 
     Optional<Object> findTop1ByRoomId_IdAndSensorTypeOrderByRecordedAtDesc(Long id, String sensorType);
 }
