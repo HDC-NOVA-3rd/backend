6fc91c6867c05c267e5c2df3cc7a9d6801bab8e7
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-02-05 18:20:17 +0900

    [CHOR] 디자이스 api 주석 추가

--- src/main/java/com/backend/nova/admin/controller/AdminOtpController.java
+++ src/main/java/com/backend/nova/admin/controller/AdminOtpController.java
@@ -8,7 +8,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/admin/otp")
+@RequestMapping("/api/admin/device")
+//@RequestMapping("/api/admin/otp")
 @RequiredArgsConstructor
 public class AdminOtpController {
 
