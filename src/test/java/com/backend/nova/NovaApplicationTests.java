5ae1a07d78d962a7a2eff5bfbacc86816e3f655c
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-02-06 15:19:21 +0900

    [CHOR] 자바메일 테스트 Mock 추가

--- src/test/java/com/backend/nova/NovaApplicationTests.java
+++ src/test/java/com/backend/nova/NovaApplicationTests.java
@@ -4,10 +4,14 @@ package com.backend.nova;
 import com.backend.nova.auth.otp.StatelessOtpService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class NovaApplicationTests {
+
+    @MockitoBean
+    private JavaMailSender javaMailSender;
 
     @MockitoBean
     private StatelessOtpService statelessOtpService;
