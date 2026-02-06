d1fec0d5699180e327ee2f97cb4c01b8d0512576
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-02-06 15:17:21 +0900

    [CHOR] OTP 테스트 Mock 추가

--- src/test/java/com/backend/nova/NovaApplicationTests.java
+++ src/test/java/com/backend/nova/NovaApplicationTests.java
@@ -1,11 +1,16 @@
 package com.backend.nova;
 
 
+import com.backend.nova.auth.otp.StatelessOtpService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class NovaApplicationTests {
+
+    @MockitoBean
+    private StatelessOtpService statelessOtpService;
 
 	@Test
 	void contextLoads() {
