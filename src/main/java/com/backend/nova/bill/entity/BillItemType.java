ed1154c4709ea322ee98746a2f7018b4bcc886a7
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-02-09 16:27:55 +0900

    [CHOR] ENUM 주석 추가

--- src/main/java/com/backend/nova/bill/entity/BillItemType.java
+++ src/main/java/com/backend/nova/bill/entity/BillItemType.java
@@ -1,7 +1,7 @@
 package com.backend.nova.bill.entity;
 
 public enum BillItemType {
-    METER,//계량기 iot센서값
-    MANAGEMENT, //관리비 기본 항목
-    COMMUNITY // 월,호별 커뮤니티 사용료
+    METER,        // 계량기 기반 요금 (전기/수도/가스)
+    MANAGEMENT,  // 관리비 기본 항목
+    COMMUNITY    // 커뮤니티 시설 이용료
 }
