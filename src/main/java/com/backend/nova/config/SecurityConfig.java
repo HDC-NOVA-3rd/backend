056ed723d23f2f224889bf42ecc1e174c6d60b59
CommandCenter <70793831+AhnCSK@users.noreply.github.com>
2026-02-11 12:39:42 +0900

    [CHOR] 멤버 디테일 롤백

--- src/main/java/com/backend/nova/config/SecurityConfig.java
+++ src/main/java/com/backend/nova/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8081"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8081", "http://localhost:8080", "http://192.168.14.168:8080"));
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
